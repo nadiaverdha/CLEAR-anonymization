d1cae7bb360ad37fc3bc313218c8d8e548558469
nadiaverdha <verdhanadia@yahoo.com>
2025-11-26 13:16:23 +0100

    added extra item labels containing spans in the dataset dict

--- clear_anonymization/ner_datasets/ler_dataset.py
+++ clear_anonymization/ner_datasets/ler_dataset.py
@@ -5,6 +5,51 @@ import torch
 from torch.utils.data import Dataset
 
 
+def tokens_to_substrs(tokens, labels):
+    substrs = {}
+    current_tokens = []
+    current_label = None
+    for token, label in zip(tokens, labels):
+        if label.startswith("B-"):
+            if current_tokens:
+                substr = " ".join(current_tokens)
+                substrs[substr] = current_label
+            current_tokens = [token]
+            current_label = label[2:]
+        elif label.startswith("I-") and current_label == label[2:]:
+            current_tokens.append(token)
+        else:
+            if current_tokens:
+                substr = " ".join(current_tokens)
+                substrs[substr] = current_label
+                current_tokens = []
+                current_label = None
+
+    if current_tokens:
+        substr = " ".join(current_tokens)
+        substrs[substr] = current_label
+
+    return substrs
+
+
+def _to_spans(substrs: dict, sentence: str):
+    spans = []
+    for sub, l in substrs.items():
+        if not sub:
+            continue
+        match = re.search(re.escape(sub), sentence)
+        if match:
+            spans.append(
+                {
+                    "start": match.start(),
+                    "end": match.end(),
+                    "text": sub,
+                    "entity": l,
+                }
+            )
+    return spans
+
+
 @dataclass
 class LERSample:
     tokens: list[str]
@@ -12,16 +57,18 @@ class LERSample:
     ner_labels: list[str]
     coarse_ner_labels: list[str]
     split: str
+    labels: list
 
     @classmethod
     def from_json(cls, json_dict: dict) -> "LERSample":
-        print(json_dict["coarse_ner_labels"])
+        substrs = tokens_to_substrs(json_dict["tokens"], json_dict["coarse_ner_labels"])
         return cls(
             tokens=json_dict["tokens"],
             sentences=json_dict["sentences"],
             ner_labels=json_dict["ner_labels"],
             coarse_ner_labels=json_dict["coarse_ner_labels"],
             split=json_dict["split"],
+            labels=_to_spans(substrs, json_dict["sentences"]),
         )
 
 
