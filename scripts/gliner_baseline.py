dd9c857e07cd9e185b7cfe3eef20901947f19864
nadiaverdha <verdhanadia@yahoo.com>
2025-12-04 11:44:35 +0100

    gliner baseline

--- scripts/gliner_baseline.py
+++ scripts/gliner_baseline.py
@@ -6,17 +6,13 @@ from pathlib import Path
 
 from gliner2 import GLiNER2
 
-from clear_anonymization.extractors import factory
-from clear_anonymization.extractors.base import BaseExtractor
-from clear_anonymization.extractors.cache import CacheManager
 from clear_anonymization.ner_datasets.ler_dataset import LERData, LERSample
 
 
 def to_spans(predicted, text):
     spans = []
-    # print(predicted)
     entities = predicted["entities"]
-    # print(entities)
+
     if entities:
         for label, subs in entities.items():
             for sub in subs:
@@ -43,7 +39,7 @@ def main():
         help="Path to the evaluation data (JSON format)",
     )
     parser.add_argument("--model", type=str, required=True, help="GliNER model ")
-    parser.add_argument("--cache_file", type=str, default=None)
+    parser.add_argument("--output_file", type=str, default=None)
 
     args = parser.parse_args()
 
@@ -56,19 +52,6 @@ def main():
         "LIT": "Rechtsliteratur (Rechtsliteratur, Gesetzgebungsmaterialien)",
         "REG": "Einzelfallregelungen (Vorschriften, Verträge)",
     }
-    if args.cache_file is None:
-        cache_file = (
-            Path(__file__).parent.parent
-            / "clear_anonymization"
-            / f"cache"
-            / "gliner"
-            / "cache.json"
-        )
-        print(f"Using default cache file: {cache_file}")
-    else:
-        print(f"Using provided cache file: {cache_file}")
-
-    cache = CacheManager(cache_file)
 
     data = LERData.from_json(json.loads(Path(args.input_dir).read_text()))
     samples = [s for s in data.samples if s.split == "validation"]
@@ -82,17 +65,15 @@ def main():
     with ThreadPoolExecutor() as ex:
         formatted_results = list(
             ex.map(
-                lambda pair: {"text": pair[0], "spans": to_spans(pair[1], pair[0])},
+                lambda pair: {"text": pair[0], "labels": to_spans(pair[1], pair[0])},
                 zip(texts, results),
             )
         )
 
-    # for text, predicted in zip(texts, results):
-    #    formatted_results.append({"text": text, "spans": to_spans(predicted, text)})
+    if not output_file:
+        output_file = Path("gliner_results.json")
 
-    output_path = Path("gliner_results.json")
-
-    output_path.write_text(json.dumps(formatted_results, indent=2, ensure_ascii=False))
+    output_file.write_text(json.dumps(formatted_results, indent=2, ensure_ascii=False))
 
 
 if __name__ == "__main__":
