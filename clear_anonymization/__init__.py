577fc1eec969a9121e01e2e84c7062f80f2e0074
nadiaverdha <verdhanadia@yahoo.com>
2026-02-05 10:25:34 +0100

    init file

--- clear_anonymization/__init__.py
+++ clear_anonymization/__init__.py
@@ -3,4 +3,4 @@ from clear_anonymization.ner_datasets import NERSample, NERData
 
 __version__ = "0.1.0"
 
-__all__ = ["clear-anonymization", "NERSample", "NERData"]
+__all__ = ["clear_anonymization", "NERSample", "NERData"]
