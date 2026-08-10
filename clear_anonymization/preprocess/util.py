41861a15fe0a28a50a97e4e7e5ba0ed2b93bb43c
nadiaverdha <verdhanadia@yahoo.com>
2026-08-10 08:31:48 +0200

    added new abbrv that should not be considered as the point to split sentences

--- clear_anonymization/preprocess/util.py
+++ clear_anonymization/preprocess/util.py
@@ -144,7 +144,21 @@ TITLES = {
     "Kff.",
     "Steuerber.",
     "Dr. med.vet.",
-    "Mag.pharm."
+    "Mag.pharm.",
+    "Dkfm.",
+    "Vert.i.Strafs.",
+    "Prof. Univ.Doz.",
+    "Dr. St.",
+    "Maga.",
+    "Drin.",
+    "Gaisbauerstr.",
+    "Komm.",
+    "KomzlR.",
+    "Dirin.",
+    "GmbH.",
+    "Reihe.",
+    "Comp.",
+    "Vert.i.Strafs.",
 }
 
 TOB = {"e.U.", "e.U", "e.G.", "e.G"}
